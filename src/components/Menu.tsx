e069947d9afc0bf46c381b8dcd739c1edbde7536
RUSHi-VAiRALE <rushivairale932@gmail.com>
2024-12-05 00:20:49 +0530

    new layout added

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -112,3 +112,40 @@ const menuItems = [
     ],
   },
 ];
+
+
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+
+function Menu() {
+  return (
+    <div className='mt-4 text-sm'>
+      {menuItems.map(i=>(
+        <div className='flex flex-col gap-2' key={i.title}>
+          <span className='hidden lg:block text-gray-400 font-light my-4'>{i.title}</span>
+          {i.items.map(item=>(
+            <div>
+              <Link 
+                href={item.href}
+                className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+                key={item.label}
+            >
+                <Image 
+                  src={item.icon}
+                  alt={item.label}
+                  width={20} 
+                  height={20}
+                />
+                <span className="hidden lg:block">{item.label}</span>
+            </Link>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
+
